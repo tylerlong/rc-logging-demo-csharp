7b81e36edc65f067a76af106ee709b3c6703e926
Tyler Long <tyler4long@gmail.com>
2022-03-17 09:55:18 -0700

    First working version

--- RcLoggingDemo/Program.cs
+++ RcLoggingDemo/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Diagnostics;
+using RingCentral;
+
+Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+
+var rc = new RestClient("fake", "fake", "https://platform.ringcentral.com");
+await rc.Authorize("fake", "", "fake");
